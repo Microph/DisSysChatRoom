330b6a8d6da03081c874bddddd09e47b0abb08cc
5730653821-SV <Suwapat.wow@gmail.com>
2017-04-22 18:59:16 +0700

    gu mai roo

--- index.js
+++ index.js
@@ -1,11 +1,85 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var redis = require('redis').createClient();
+var port = process.env.PORT || 8080;
+server.listen(port);
 
-app.get('/', function(req, res){
+var messages=[];
+var onlineUser=[];
+var user=[];
+/*app.get('/', function(req, res){
   res.render(__dirname + '/index.ejs');
-});
+});*/
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+//require('./config')(app, io);
+require('./routes')(app, io);
+console.log("Server has started on port " + port);
+
+
+var storeMessage = function(name, data) {
+    var message = JSON.stringify({
+        name: name,
+        data: data
+    });
+
+    redisClient.lpush("messages", message, function(err, response) {
+        redisClient.ltrim("messages", 0, 10);
+        console.log(response);
+    });
+};
+
+var chatRoom = io.on('connection',function(socket){
+	console.log('Client connected');
+
+	socket.on('login',function(username){
+		socket.set('username',username);
+		redis.sadd("users",username);
+
+		/*redis.smembers('groups'+username, function(err,groups) {
+            console.log("groups: " + groups);
+
+            groups.forEach(function(group) {
+                socket.emit('add group', group);
+            });
+        });*/
+	});
+
+	socket.on('join',function(socket){
+		/*socket.broadcast.emit('message',socket.username +  "has joined the chat room");
+
+		redisClient.smembers('chatters', function(err, names) {
+            console.log("names: " + names);
+
+            names.forEach(function(name) {
+                socket.emit('add user', name);
+            });
+        });*/
+
+		
+	});
+
+	socket.on('createGroup',function(groupID){
+
+		/*redis.sismember('groups',groupID,function(err,reply){
+			if(reply == 0){	
+                redis.sadd('groups',groupID);
+                socket.emit('add group', groupID);
+			}
+
+			else{
+				socket.emit('error',"Duplicate group ID");
+			} //emit-> alert
+
+		});*/
+		
+	});
+
+	socket.on('disconnect',function(socket){
+		//remove from online set
+	});
+
+	socket.on('message',function(data){
+
+	});
 });
--- routes.js
+++ routes.js
@@ -1,5 +1,26 @@
-const express = require('express');
+//const express = require('express');
 
-const router = express.Router();
+//const router = express.Router();
 
-module.exports = router;
+module.exports = function(app,io){
+
+	app.get('/',function(request,response){
+		response.render(__dirname + '/index.ejs');
+	});
+
+	app.get('/login',function(request,response){
+		response.redirect('/chat');
+	});
+
+	app.get('/chat',function(request,response){
+		response.render(__dirname + '/chat.ejs');
+	});
+
+};
+
+
+
+
+
+
+

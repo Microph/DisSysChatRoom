c78f6c2f3527d6ca4307a3fb55054fce935ffc32
Mickke <mikaecat@gmail.com>
2017-04-22 15:53:49 +0700

    Change pages to ejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render(__dirname + '/index.ejs');
 });
 
 http.listen(3000, function(){

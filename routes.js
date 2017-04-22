4e4c466cf99d79b74cd111c73cd13c6bc254018a
5730653821-SV <Suwapat.wow@gmail.com>
2017-04-22 21:50:37 +0700

    undefine/id

--- routes.js
+++ routes.js
@@ -3,16 +3,28 @@
 //const router = express.Router();
 
 module.exports = function(app,io){
+	//var username = Number(window.location.pathname.match(/\/chat\/(\d+)$/)[1]);
 
 	app.get('/',function(request,response){
 		response.render(__dirname + '/index.ejs');
 	});
 
 	app.get('/login',function(request,response){
-		response.redirect('/chat');
+		var username = request.query.username;
+		response.redirect('/chat/'+username);
 	});
 
-	app.get('/chat',function(request,response){
+	app.get('/chat/:username',function(request,response){
+		response.render(__dirname + '/chat.ejs');
+	});
+
+	app.get('/loginRoom',function(request,response){
+		var username = request.query.username;
+		var id = request.query.room_name;
+		response.redirect('/chat/'+username+'/'+id);
+	});
+
+	app.get('/chat/:username/:id',function(request,response){
 		response.render(__dirname + '/chat.ejs');
 	});
 

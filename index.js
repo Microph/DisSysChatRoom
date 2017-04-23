15ad647c6cb5305b667488c18d1d9360ca040964
Mickke <mikaecat@gmail.com>
2017-04-23 13:46:01 +0700

    Add chat room ui

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.render(__dirname + '/index.ejs');
+  res.render(__dirname + '/views/index.ejs');
 });
+
+app.get('/chat', function(req, res){
+  res.render(__dirname + '/views/chat.ejs', {
+          username: req.query.username
+        });
+});
+
+app.use(express.static(__dirname + '/public'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
